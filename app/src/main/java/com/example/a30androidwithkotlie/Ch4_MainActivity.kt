27d155f505eea4e2a3d47fa9481f6e556a61884c
Cjsghkd <pjhomg5530@naver.com>
2022-07-26 14:43:50 +0900

    계산기 UI - 02

--- app/src/main/java/com/example/a30androidwithkotlie/Ch4_MainActivity.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch4_MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.a30androidwithkotlie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Ch4_MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ch4_main)
+    }
+    fun buttonClicked(v : View) {
+
+    }
+
+    fun resultButtonClicked(v : View) {
+
+    }
+
+    fun historyButtonClicked(v : View) {
+
+    }
+
+    fun clearButtonClicked(v : View) {
+
     }
 }
