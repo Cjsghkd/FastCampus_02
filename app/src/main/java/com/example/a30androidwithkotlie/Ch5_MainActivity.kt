f0c1c746dd4a125b9c3a6f05c31bb87e788c8aba
Cjsghkd <pjhomg5530@naver.com>
2022-07-29 06:14:16 +0900

    Ch5_전자액자 - 03

--- app/src/main/java/com/example/a30androidwithkotlie/Ch5_MainActivity.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch5_MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.a30androidwithkotlie
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -30,6 +32,8 @@ class Ch5_MainActivity : AppCompatActivity() {
             add(findViewById(R.id.imageView23))
         }
     }
+
+    private val imageUriList : MutableList<Uri> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -86,6 +90,35 @@ class Ch5_MainActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         startActivityForResult(intent, 2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when (requestCode) {
+            2000 -> {
+                val selectImageUri : Uri? = data?.data
+                if (selectImageUri != null) {
+
+                    if (imageUriList.size == 6) {
+                        Toast.makeText(this, "이미 사진이 꽉 찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun showPermissionContextPopup() {
