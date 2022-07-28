9c65bcc6c3b4ca640c084b940944662aa5fc955a
Cjsghkd <pjhomg5530@naver.com>
2022-07-29 06:04:57 +0900

    Ch5_전자액자 - 02

---
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch5_MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.a30androidwithkotlie
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+
+class Ch5_MainActivity : AppCompatActivity() {
+
+    val addPhotoButton : Button by lazy {
+        findViewById(R.id.addPhotoButton)
+    }
+
+    private val startPhotoFrameModeButton : Button by lazy {
+        findViewById(R.id.startPhotoFrameModeButton)
+    }
+
+    private val imageViewList : List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ch5_main)
+
+        initAddPhotoButton()
+        initStartPhotoFrameModeButton()
+
+    }
+
+    private fun initAddPhotoButton() {
+        addPhotoButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    // 교육용 팝업 확인 후 권환 팝업을 띄우는 기능능
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+           }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한이 부여된 것입니다.
+                    navigatePhotos()
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> { }
+        }
+
+    }
+
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("전자액자에 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의하기", { dialog, which ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            })
+            .setNegativeButton("취소하기", { dialog, which -> })
+            .create()
+            .show()
+    }
+
+    private fun initStartPhotoFrameModeButton() {
+
+    }
+
+}
