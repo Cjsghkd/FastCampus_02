c5a70c4197858098121157e14a095c38dfb8f64b
Cjsghkd <pjhomg5530@naver.com>
2022-07-29 06:17:31 +0900

    Ch5_전자액자 - 03

--- app/src/main/java/com/example/a30androidwithkotlie/Ch5_MainActivity.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch5_MainActivity.kt
@@ -81,7 +81,7 @@ class Ch5_MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
-            else -> { }
+            else -> {}
         }
 
     }
@@ -125,10 +125,10 @@ class Ch5_MainActivity : AppCompatActivity() {
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
             .setMessage("전자액자에 앱에서 사진을 불러오기 위해 권한이 필요합니다.")
-            .setPositiveButton("동의하기", { dialog, which ->
+            .setPositiveButton("동의하기") { _, _ ->
                 requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
-            })
-            .setNegativeButton("취소하기", { dialog, which -> })
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
             .create()
             .show()
     }
