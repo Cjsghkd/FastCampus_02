7d17dab75648fc8e6980fbd866775a126a10abfa
Cjsghkd <pjhomg5530@naver.com>
2022-08-04 05:03:47 +0900

    Ch7_녹음기 - 03

--- app/src/main/java/com/example/a30androidwithkotlie/Ch7_MainActivity.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch7_MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.a30androidwithkotlie
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -12,8 +14,20 @@ class Ch7_MainActivity : AppCompatActivity() {
     }
 
     private var state = Ch7_State.BEFORE_RECORDING
+        set(value) {
+            field = value
+            recordButton.updateIconWithState(value)
+    }
 
     private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    private var recorder : MediaRecorder? = null
+
+    private val recordingFilePath : String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var player : MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +59,61 @@ class Ch7_MainActivity : AppCompatActivity() {
 
     private fun initView() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        state = Ch7_State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        state = Ch7_State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            prepare()
+        }
+        player?.start()
+        state = Ch7_State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = Ch7_State.AFTER_RECORDING
+    }
+
+    private fun bindViews() {
+        recordButton.setOnClickListener {
+            when(state) {
+                Ch7_State.BEFORE_RECORDING -> {
+                    startRecording()
+                }
+                Ch7_State.ON_RECORDING -> {
+                    stopRecording()
+                }
+                Ch7_State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+                Ch7_State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
     }
 
     companion object {
