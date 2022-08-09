8e143150892ede4bb8e2e5e7763c2f8bf8531de8
Cjsghkd <pjhomg5530@naver.com>
2022-08-09 19:47:41 +0900

    Ch9_푸시 알림 수신기 - 05

--- app/src/main/java/com/example/a30androidwithkotlie/Ch9_MainActivity.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch9_MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.a30androidwithkotlie
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -21,6 +23,14 @@ class Ch9_MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ch9_main)
 
         initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)
+        updateResult(true)
     }
 
     private fun initFirebase() {
@@ -29,6 +39,16 @@ class Ch9_MainActivity : AppCompatActivity() {
                 val token = task.result
                 firebaseToken.text = task.result
             }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent : Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                 } else {
+                    "(으)로 실행했습니다."
         }
     }
 }
