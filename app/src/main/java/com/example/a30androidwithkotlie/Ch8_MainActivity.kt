f89223ac20fc7e72d1d8bc224d18b82eefa5d444
Cjsghkd <pjhomg5530@naver.com>
2022-08-09 11:42:03 +0900

    Ch8_심플 웹 브라우저 - 02

--- app/src/main/java/com/example/a30androidwithkotlie/Ch8_MainActivity.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch8_MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.a30androidwithkotlie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class Ch8_MainActivity : AppCompatActivity() {
+
+    private val webview : WebView by lazy {
+        findViewById(R.id.WebView)
+    }
+
+    private val addressBar : EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ch8_main)
+
+        initViews()
+        bindViews()
     }
+
+    private fun initViews() {
+        webview.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+//        webview.webViewClient = WebViewClient()
+//        webview.settings.javaScriptEnabled = true
+//        webview.loadUrl("http://www.google.com")
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId , event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webview.loadUrl(v.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
+    }
+
 }
