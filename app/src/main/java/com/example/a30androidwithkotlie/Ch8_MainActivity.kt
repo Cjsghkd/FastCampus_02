c80b535e30c14b3db4ef872ad13d8495698bf53b
Cjsghkd <pjhomg5530@naver.com>
2022-08-09 11:21:59 +0900

    Ch8_심플 웹 브라우저 - 01

---
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch8_MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a30androidwithkotlie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ch8_MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ch8_main)
+    }
+}
