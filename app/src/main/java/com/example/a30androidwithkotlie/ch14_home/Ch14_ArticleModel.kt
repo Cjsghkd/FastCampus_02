8b4ec5127c6f6e8accfee9e247efb60bc462547e
Cjsghkd <pjhomg5530@naver.com>
2022-08-11 15:56:12 +0900

    Ch14_중고거래 앱 - 02

---
+++ app/src/main/java/com/example/a30androidwithkotlie/ch14_home/Ch14_ArticleModel.kt
@@ -0,0 +1,9 @@
+package com.example.a30androidwithkotlie.ch14_home
+
+data class Ch14_ArticleModel(
+    val sellerId : String,
+    val title : String,
+    val createdAt : Long,
+    val price : String,
+    val imageUrl : String
+)
