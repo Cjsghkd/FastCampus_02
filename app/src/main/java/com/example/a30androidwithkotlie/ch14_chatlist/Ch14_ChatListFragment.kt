a641bdf1a0706c86dde1723636c0fb09965b79b3
Cjsghkd <pjhomg5530@naver.com>
2022-08-21 19:16:47 +0900

    Ch14_중고거래 앱

--- app/src/main/java/com/example/a30androidwithkotlie/ch14_chatlist/Ch14_ChatListFragment.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/ch14_chatlist/Ch14_ChatListFragment.kt
@@ -4,5 +4,4 @@ import androidx.fragment.app.Fragment
 import com.example.a30androidwithkotlie.R
 
 class Ch14_ChatListFragment : Fragment(R.layout.ch14_fragment_chatlist) {
-
 }
--- app/src/main/java/com/example/a30androidwithkotlie/ch14_mypage/Ch14_MyPageFragment.kt
+++ app/src/main/java/com/example/a30androidwithkotlie/ch14_mypage/Ch14_MyPageFragment.kt
@@ -4,5 +4,4 @@ import androidx.fragment.app.Fragment
 import com.example.a30androidwithkotlie.R
 
 class Ch14_MyPageFragment : Fragment(R.layout.ch14_fragment_mypage) {
-
 }
