f956fda76c623c23e9908149d79cdb2bd00ef0d5
Cjsghkd <pjhomg5530@naver.com>
2022-08-22 19:06:08 +0900

    Ch15 유튜브 - 0

---
+++ app/src/main/java/com/example/a30androidwithkotlie/Ch15_MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a30androidwithkotlie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ch15_MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ch15_main)
+
+
+
+    }
+}
